3c9385c8a1542a1f6b62773a377d40619bfdf506
Louis Greiner <57864277+louisgreiner@users.noreply.github.com>
2021-06-04 08:16:49 +0200

    Final update

--- tp4.c
+++ tp4.c
@@ -5,28 +5,26 @@
 #include <stdio.h>
 #include "tp4.h"
 
-// 1) Ajouter un nombre de vaccin dans une liste de vaccins donnée, pour une marque donnée
+// 1) Ajouter un nombre de vaccin dans une liste de vaccins donne, pour une marque donne
 void ajouterVaccinL(T_ListeVaccins** listeVaccins, char* marque, int nb_vaccins) {
-    if (*listeVaccins == NULL) {     //liste nulle / arrivé en bout de liste sans insérer -> on insère un nouveau maillon
+    if (*listeVaccins == NULL) {     //liste nulle / arriv en bout de liste sans insrer -> on insre un nouveau maillon
         *listeVaccins = malloc(sizeof(T_ListeVaccins));
         (*listeVaccins)->nombre_vaccins = nb_vaccins;
         (*listeVaccins)->marque = malloc(51 * sizeof(char));
         strcpy((*listeVaccins)->marque, marque);
         (*listeVaccins)->suivant = NULL;
     }
-    else if (strcmp((*listeVaccins)->marque, marque) == 0) {      //marque existante : on ajoute les vaccins à son total de vaccins
+    else if (strcmp((*listeVaccins)->marque, marque) == 0) {      //marque existante : on ajoute les vaccins  son total de vaccins
         (*listeVaccins)->nombre_vaccins += nb_vaccins;
     }
-    else {      //pas de correspondance, pas en bout de liste -> on passe à l'élément suivant
-       //printf("Marque parcourue : %s\n", (*listeVaccins)->marque);
+    else {      //pas de correspondance, pas en bout de liste -> on passe  l'lment suivant
         ajouterVaccinL(&(*listeVaccins)->suivant, marque, nb_vaccins);
     }
 }
 
-// 2) Ajouter un nombre de vaccin dans un ABR, pour une marque à une date donnée
+// 2) Ajouter un nombre de vaccin dans un ABR, pour une marque  une date donne
 void ajouterVaccinA(T_ABR** abr, char* date, char* marque, int nb_vaccins) {
-    if (*abr == NULL) {      //arbre nul / pas inséré au niveau des feuilles -> nouvelle feuille
-        //printf("\tNouveau noeud\n");
+    if (*abr == NULL) {      //arbre nul / pas insr au niveau des feuilles -> nouvelle feuille
         *abr = (T_ABR*)malloc(sizeof(T_ABR));
         (*abr)->date = (char*)malloc(11 * sizeof(char));
         (*abr)->liste_vaccins = NULL;
@@ -35,19 +33,15 @@ void ajouterVaccinA(T_ABR** abr, char* date, char* marque, int nb_vaccins) {
         (*abr)->fils_gauche = NULL;
         ajouterVaccinL(&(*abr)->liste_vaccins, marque, nb_vaccins);
     }
-    else if (comparerDates(date, (*abr)->date) > 0) {       //clé à insérer supérieure à clé parcourue -> insertion dans arbre droit
-        //printf("\tNoeud droit\n");
+    else if (comparerDates(date, (*abr)->date) > 0) {       //cl  insrer suprieure  cl parcourue -> insertion dans arbre droit
         ajouterVaccinA(&(*abr)->fils_droit, date, marque, nb_vaccins);
     }
-    else if (comparerDates(date, (*abr)->date) < 0) {       //clé à insérer inférieure à clé parcourue -> insertion dans arbre gauche
-        //printf("\tNoeud gauche\n");
+    else if (comparerDates(date, (*abr)->date) < 0) {       //cl  insrer infrieure  cl parcourue -> insertion dans arbre gauche
         ajouterVaccinA(&(*abr)->fils_gauche, date, marque, nb_vaccins);
     }
-    else {      //clé à insérer égale à clé parcourue : on ajoute les vaccins au noeud parcouru
-        //printf("\tNoeud trouve\n");
+    else {      //cl  insrer gale  cl parcourue : on ajoute les vaccins au noeud parcouru
         ajouterVaccinL(&(*abr)->liste_vaccins, marque, nb_vaccins);
     }
-    //printf("Nb vaccins %s recu le %s : %d\n",(*abr)->liste_vaccins->marque, (*abr)->date, (*abr)->liste_vaccins->nombre_vaccins);
 }
 
 // 3.1) Afficher tous les stocks disponibles pour un ABR par ordre croissant de date, affichage en ligne
@@ -55,7 +49,7 @@ void afficherStockL(T_ListeVaccins* listeVaccins)
 {
     T_ListeVaccins* temp_listeVaccins = listeVaccins;
     while (temp_listeVaccins != NULL) {
-        if (temp_listeVaccins->nombre_vaccins > 0) { // test pas forcément utile
+        if (temp_listeVaccins->nombre_vaccins > 0) { // test pas forcment utile
             printf("\n\t- %s (%d vaccins)", temp_listeVaccins->marque, temp_listeVaccins->nombre_vaccins);
         }
         temp_listeVaccins = temp_listeVaccins->suivant;
@@ -63,7 +57,7 @@ void afficherStockL(T_ListeVaccins* listeVaccins)
     printf("\n");
 }
 
-// 4.1) Afficher tous les stocks disponibles dans un ABR pour une marque donnée, affichage en ligne
+// 4.1) Afficher tous les stocks disponibles dans un ABR pour une marque donne, affichage en ligne
 void afficherStockA(T_ABR* abr)
 {
     if (abr == NULL) {
@@ -85,7 +79,7 @@ void afficherStockLArbre(T_ListeVaccins* listeVaccins, int hauteur)
 {
     T_ListeVaccins* temp_listeVaccins = listeVaccins;
     while (temp_listeVaccins != NULL) {
-        if (temp_listeVaccins->nombre_vaccins > 0) { // test pas forcément utile
+        if (temp_listeVaccins->nombre_vaccins > 0) { // test pas forcment utile
             for (int i = 0; i < hauteur; i++) {
                 printf("\t\t");
             }
@@ -96,7 +90,7 @@ void afficherStockLArbre(T_ListeVaccins* listeVaccins, int hauteur)
     printf("\n");
 }
 
-// 4.2) Afficher tous les stocks disponibles dans un ABR pour une marque donnée, affichage en arbre
+// 4.2) Afficher tous les stocks disponibles dans un ABR pour une marque donne, affichage en arbre
 void afficherStockAArbre(T_ABR* abr, int hauteur)
 {
     if (abr == NULL) {
@@ -117,12 +111,12 @@ void afficherStockAArbre(T_ABR* abr, int hauteur)
     }
 }
 
-// 5) Compter le nombre de vaccins disponibles dans un ABR pour une marque donnée
+// 5) Compter le nombre de vaccins disponibles dans un ABR pour une marque donne
 int compterVaccinsListe(T_ListeVaccins* listeVaccins, char* marque)
 {
     T_ListeVaccins* temp_listeVaccins = listeVaccins;
     while (temp_listeVaccins != NULL) {
-        if (strcmp(temp_listeVaccins->marque, marque) == 0) { // on a trouvé un vaccin de la bonne marque
+        if (strcmp(temp_listeVaccins->marque, marque) == 0) { // on a trouv un vaccin de la bonne marque
             return temp_listeVaccins->nombre_vaccins;
         }
         temp_listeVaccins = temp_listeVaccins->suivant;
@@ -136,7 +130,7 @@ int compterVaccins(T_ABR* abr, char* marque)
         return 0;
     }
     int total = 0;
-    // On parcourt l'arbre en infixe pour parcourir tous les noeuds (être sûr d'en oublier aucun)
+    // On parcourt l'arbre en infixe pour parcourir tous les noeuds (tre sr d'en oublier aucun)
     if (abr->fils_gauche != NULL) { // fils gauche existe, on y va
         total += compterVaccins(abr->fils_gauche, marque);
     }
@@ -148,14 +142,14 @@ int compterVaccins(T_ABR* abr, char* marque)
     return(total);
 }
 
-// 6) Déduire un nombre de vaccins d'une marque donnée dans une liste de vaccins
+// 6) Dduire un nombre de vaccins d'une marque donne dans une liste de vaccins
 void deduireVaccinL(T_ListeVaccins** listeVaccins, char* marque, int nb_vaccins)
 {
     if (*listeVaccins == NULL) { // cas liste nulle (ne devrait pas arriver)
         printf("impossible");
         return;
     }
-    if (strcmp((*listeVaccins)->marque, marque) == 0) { // si la bonne marque se trouve en début de liste
+    if (strcmp((*listeVaccins)->marque, marque) == 0) { // si la bonne marque se trouve en dbut de liste
         if ((*listeVaccins)->nombre_vaccins <= nb_vaccins) { // on veut retirer autant ou plus de vaccins que ce qu'il y en a => suppression du maillon
             (*listeVaccins) = (*listeVaccins)->suivant;
             return;
@@ -166,14 +160,15 @@ void deduireVaccinL(T_ListeVaccins** listeVaccins, char* marque, int nb_vaccins)
         }
     }
 
-    T_ListeVaccins* temp_listeVaccins = (*listeVaccins); // peut-être : "temp_listeVaccins = listeVaccins->suivant" pour optimiser et gagner 1 opération
+    T_ListeVaccins* temp_listeVaccins = (*listeVaccins); // peut-tre : "temp_listeVaccins = listeVaccins->suivant" pour optimiser et gagner 1 opration
     T_ListeVaccins* temp_listeVaccins_precedent = (*listeVaccins);
 
-    // si la bonne marque ne se trouve pas en début de liste
+    // si la bonne marque ne se trouve pas en dbut de liste
     while (temp_listeVaccins != NULL) {
-        if (strcmp(temp_listeVaccins->marque, marque) == 0) { // on a trouvé un vaccin de la bonne marque
+        if (strcmp(temp_listeVaccins->marque, marque) == 0) { // on a trouv un vaccin de la bonne marque
             if (temp_listeVaccins->nombre_vaccins <= nb_vaccins) { // on veut retirer autant ou plus de vaccins que ce qu'il y en a => suppression du maillon
                 temp_listeVaccins_precedent->suivant = temp_listeVaccins->suivant;
+                free(temp_listeVaccins->marque);
                 free(temp_listeVaccins);
                 return;
             }
@@ -188,20 +183,17 @@ void deduireVaccinL(T_ListeVaccins** listeVaccins, char* marque, int nb_vaccins)
 }
 
 
-
-//TODO : vérifier d'abord nombre
-
 void deduireVaccinA(T_ABR** abr, char* marque, int nb_vaccins) {
     if (*abr == NULL) {
         return;
     }
-    //On déduit les vaccins du sous-arbre gauche
+    //On dduit les vaccins du sous-arbre gauche
     if ((*abr)->fils_gauche != NULL) {
         int temp_nb = compterVaccins((*abr)->fils_gauche, marque);
         deduireVaccinA(&(*abr)->fils_gauche, marque, nb_vaccins);
         nb_vaccins -= temp_nb - compterVaccins((*abr)->fils_gauche, marque);
     }
-    if (nb_vaccins > 0) { //Il reste des vaccins à déduire => on déduit dans le noeud parcouru
+    if (nb_vaccins > 0) { //Il reste des vaccins  dduire => on dduit dans le noeud parcouru
         int temp_nb = compterVaccinsListe((*abr)->liste_vaccins, marque);
         deduireVaccinL(&(*abr)->liste_vaccins, marque, nb_vaccins);
         int total_noeud_courant = compterVaccinsListe((*abr)->liste_vaccins, marque);
@@ -211,14 +203,14 @@ void deduireVaccinA(T_ABR** abr, char* marque, int nb_vaccins) {
             supprimerNoeud(abr, (*abr)->date);
         }
     }
-    if (*abr!=NULL && nb_vaccins > 0 && (*abr)->fils_droit != NULL) { //Il reste des vaccins à déduire => on déduit dans le sous-arbre droit
+    if (*abr!=NULL && nb_vaccins > 0 && (*abr)->fils_droit != NULL) { //Il reste des vaccins  dduire => on dduit dans le sous-arbre droit
         deduireVaccinA(&(*abr)->fils_droit, marque, nb_vaccins);
     }
-    //À ce moment, les vaccins ont été déduits dans tout le sous arbre passé en paramètre.
-    //S'il reste des vaccins à déduire dans l'arbre, alors ils le seront dans l'appel de fonction ayant appelé l'itération courante
+    // ce moment, les vaccins ont t dduits dans tout le sous arbre pass en paramtre.
+    //S'il reste des vaccins  dduire dans l'arbre, alors ils le seront dans l'appel de fonction ayant appel l'itration courante
 }
 
-// Renvoie le noeud dont la date (= clé) est la plus petite de l'arbre après celle entrée en paramètre
+// Renvoie le noeud dont la date (= cl) est la plus petite de l'arbre aprs celle entre en paramtre
 T_ABR** plusPetiteDate(T_ABR** abr, char* date)
 {
     T_ABR** temp_abr = abr;
@@ -247,45 +239,60 @@ T_ABR** successeur(T_ABR* abr) {
 }
 
 /*
-Supprime un noeud de date donnée dans l'arbre donné
+Supprime un noeud de date donne dans l'arbre donn
 */
 void supprimerNoeud(T_ABR** abr, char* date)
 {
+
     if (*abr == NULL) {
-        //printf("Nul!\n");
         return;
     }
-    // si la date en entrée est plus grande que celle du noeud actuel
+    // si la date en entre est plus grande que celle du noeud actuel
     if (comparerDates(date, (*abr)->date) > 0) {
-        supprimerNoeud(&((*abr)->fils_droit), date); // ou supprimerNoeud(&(*abr)->fils_droit, date);
+        supprimerNoeud(&((*abr)->fils_droit), date);
         return;
     }
     // sinon
     if (comparerDates(date, (*abr)->date) < 0) {
-        supprimerNoeud(&((*abr)->fils_gauche), date); // ou supprimerNoeud(&(*abr)->fils_gauche, date);
+        supprimerNoeud(&((*abr)->fils_gauche), date);
         return;
     }
 
+    //On ne librera rellement de noeud que si le noeud  supprimer a zro ou un fils
     if ((*abr)->fils_gauche == NULL) {
-        free((*abr)->liste_vaccins);
         free((*abr)->date);
-        T_ABR* fils_droit = (*abr)->fils_droit; 
+        T_ABR* fils_droit = (*abr)->fils_droit;
+        while ((*abr)->liste_vaccins != NULL) {
+            deduireVaccinL(&(*abr)->liste_vaccins, (*abr)->liste_vaccins->marque, (*abr)->liste_vaccins->nombre_vaccins);
+        }
         free(*abr);
         *abr = fils_droit;
+
     }else if((*abr)->fils_droit == NULL){
-        free((*abr)->liste_vaccins);
-        free((*abr)->date);     //je sais pas pourquoi ça plante là, la réponse demain... euh aujourd'hui du coup
+        free(((*abr)->date));
         T_ABR* fils_gauche = (*abr)->fils_gauche;
+        while ((*abr)->liste_vaccins != NULL) {
+            deduireVaccinL(&(*abr)->liste_vaccins, (*abr)->liste_vaccins->marque, (*abr)->liste_vaccins->nombre_vaccins);
+        }
         free(*abr);
+
         *abr = fils_gauche;
     }
     else {
+        //Dans le cas o le noeud a deux racines, on ne librera pas immdiatement de noeud :
+        //On remplacera le noeud  supprimer par son successeur et on supprimera le successeur recopi (soit plus bas dans l'arbre)
+        //La libration de mmoire aura donc lieu  ce moment
         T_ABR** succ = successeur(*abr);
-        free((*abr)->liste_vaccins);
-        free((*abr)->date);
-        strcpy((*abr)->date,(*succ)->date);
-        strcpy((*abr)->liste_vaccins, (*succ)->liste_vaccins);
-        supprimerNoeud(&succ, (*succ)->date);
+        while ((*abr)->liste_vaccins != NULL){
+            free((*abr)->liste_vaccins->marque);
+            T_ListeVaccins* temp_liste = (*abr)->liste_vaccins;
+            (*abr)->liste_vaccins = (*abr)->liste_vaccins->suivant;
+            free(temp_liste);
+        }
+        strcpy((*abr)->date, (*succ)->date);
+        (*abr)->liste_vaccins = (*succ)->liste_vaccins;
+        supprimerNoeud(succ, (*succ)->date);
+
     }
 
 }
@@ -302,23 +309,23 @@ void viderBuffer()
 }
 
 /*
-ATTENTION ! Il faut bien vérifier le format des deux dates avant d'appeler cette fonction!
-Compare deux dates : si la première est supérieure à la deuxième, renvoie 1 ; si elle est inférieure à la deuxième, renvoie 2 ; et renvoie 0 en cas d'égalité
-Cette comparaison est possible de cette manière car les dates sont en format YYYY-MM-DD (ou AAAA-MM-JJ)
-En effet, les premiers caractères de la date sont les plus significatifs, et les derniers sont les moins significatifs
-Les tirets séparant années, mois et jours n'auront pas d'incidence sur le résultat, car ils sont toujours égaux entre eux
+ATTENTION ! Il faut bien vrifier le format des deux dates avant d'appeler cette fonction!
+Compare deux dates : si la premire est suprieure  la deuxime, renvoie 1 ; si elle est infrieure  la deuxime, renvoie 2 ; et renvoie 0 en cas d'galit
+Cette comparaison est possible de cette manire car les dates sont en format YYYY-MM-DD (ou AAAA-MM-JJ)
+En effet, les premiers caractres de la date sont les plus significatifs, et les derniers sont les moins significatifs
+Les tirets sparant annes, mois et jours n'auront pas d'incidence sur le rsultat, car ils sont toujours gaux entre eux
 */
 int comparerDates(char* date1, char* date2) {
-    //On compare chaque caractère de chaque chaîne
+    //On compare chaque caractre de chaque chane
     for (int i = 0; i < 10; i++) {
         if (date1[i] > date2[i]) {
-            return(1);   //un chiffre est supérieur dans la date 1 => la date 1 est supérieure à la 2
+            return(1);   //un chiffre est suprieur dans la date 1 => la date 1 est suprieure  la 2
         }
         else if (date1[i] < date2[i]) {
-            return(-1);  //un chiffre est inférieur dans la date 1 => la date 1 est inférieure à la 2
+            return(-1);  //un chiffre est infrieur dans la date 1 => la date 1 est infrieure  la 2
         }
     }
-    return 0;       //aucune différence trouvée => les dates sont identiques
+    return 0;       //aucune diffrence trouve => les dates sont identiques
 }
 
 int verifValidDate(char* date) {
@@ -342,19 +349,19 @@ int verifValidDate(char* date) {
             }
         }
     }
-    if (date[5] > '1') {                        //limitation de la dizaine du mois à 1 (pour 10)
+    if (date[5] > '1') {                        //limitation de la dizaine du mois  1 (pour 10)
         return 0;
     }
-    if (date[5] == '1' && date[6] > 2) {        //limitation du mois à 12
+    if (date[5] == '1' && date[6] > 2) {        //limitation du mois  12
         return 0;
     }
-    if (date[8] > '3') {                        //limitation de la dizaine du jour à 3 (pour 30)
+    if (date[8] > '3') {                        //limitation de la dizaine du jour  3 (pour 30)
         return 0;
     }
-    if (date[8] == '3' && date[9] > 1) {        //limitation du jour à 31
+    if (date[8] == '3' && date[9] > 1) {        //limitation du jour  31
         return 0;
     }
-    //TODO : ajouter vérification poussée (ex : année bissextile, 31 février, ...)
-    
-    return 1;   //arrivé ici, la date a passé toutes les vérifications avec succès : elle est donc valide (bien que pas forcément cohérente!)
+    //TODO : ajouter vrification pousse (ex : anne bissextile, 31 fvrier, ...)
+
+    return 1;   //arriv ici, la date a pass toutes les vrifications avec succs : elle est donc valide (bien que pas forcment cohrente!)
 }
